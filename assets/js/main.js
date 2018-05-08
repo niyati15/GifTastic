5df6e100cc3784480d295b95e7c47e7a500cc5cf
niyati shah <niyatishah@niyatis-MacBook-Pro.local>
2018-05-08 09:52:55 -0700

    added more logic

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,62 @@
 console.log("Hello");
-var movies = ["Minions", "Kungfu Panda", "Avengers", "The Lion King"];
+var characters = ["Harry Potter", "Ron Weasley", "Hermione Granger"];
 function renderButtons() {
-    $("#movies-view").empty();
-    for (var i = 0; i < movies.length; i++) {
-        $('#movies-view').append('<button type="button" class="btn btn-dark">' + movies[i] + '</button>&nbsp;&nbsp;').trigger('create');
-    }
+  $("#buttons-view").empty();
+  for (var i = 0; i < characters.length; i++) {
+    var a = $("<button>");
+    // Adds a class of character to our button
+    a.addClass("character");
+    // Added a data-attribute
+    a.attr("data-name", characters[i]);
+    // Provided the initial button text
+    a.text(characters[i]);
+    // Added the button to the buttons-view div
+    $("#buttons-view").append(a);
+  }
 }
 
-$("#add-movie").on("click", function (event) {
-    event.preventDefault();
-    var movieAdded = $("#movie-input").val();
-    console.log(movieAdded);
-    movies.push(movieAdded);
-    console.log(movies);
-    this.form.reset();
-    renderButtons();
+$("#add-character").on("click", function (event) {
+  event.preventDefault();
+  var characterAdded = $("#character-input").val();
+  console.log(characterAdded);
+  characters.push(characterAdded);
+  console.log(characters);
+  this.form.reset();
+  renderButtons();
+  console.log("characterAdded:", characterAdded);
+
+
+
 });
+
+function displaycharacterInfo() {
+
+  var character = $(this).attr("data-name");
+  console.log(character);
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+    character + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function (response) {
+    console.log(response);
+    var results = response.data;
+    for (var i = 0; i < results.length; i++) {
+      var gifDiv = $("<div class='item'>");
+
+      var personImage = $("<img>");
+      personImage.attr("src", results[i].embed_url);
+
+      // gifDiv.prepend(p);
+      gifDiv.prepend(personImage);
+
+      $("#characters-view").prepend(gifDiv);
+    }
+  });
+
+}
+$(document).on("click", ".character", displaycharacterInfo);
+
 renderButtons();
+
